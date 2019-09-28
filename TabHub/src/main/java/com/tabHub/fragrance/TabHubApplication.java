9df73e8beccdcdf472b400a6cb570f41a3b55984
SongHae <thdgo456@gmail.com>
2019-09-28 17:21:55 +0900

    first commit

---
+++ TabHub/src/main/java/com/tabHub/fragrance/TabHubApplication.java
@@ -0,0 +1,13 @@
+package com.tabHub.fragrance;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TabHubApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TabHubApplication.class, args);
+	}
+
+}
