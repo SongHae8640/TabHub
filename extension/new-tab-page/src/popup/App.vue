b7b14696f447967f8818ba3621ec4331f7e05f27
SongHae <thdgo456@gmail.com>
2019-10-19 12:05:51 +0900

    create tabs menu
    탭 메뉴를 즐겨찾기, 보관함, 메세지로 구분
    각 메뉴를 클릭했을때 아래 내용이 변경

--- extension/new-tab-page/src/popup/App.vue
+++ extension/new-tab-page/src/popup/App.vue
@@ -1,19 +1,51 @@
 <template>
   <div>
-    <p>Hello world!</p>
+  	<div class="container" id="tabs">
+    	<tabs-menu v-bind:tabsMenu="tabsMenu" v-bind:selectedTabsMenu="selectedTabsMenu" v-on:@change="onClickTabsMenu"></tabs-menu>
+  	</div>
+  	<div class="content">
+  		<div v-if="selectedTabsMenu === tabsMenu[2]">
+  			메세지
+  		</div>
+  		<div v-else>
+  			<div v-if="selectedTabsMenu == tabsMenu[0]">
+		  		즐겨찾기
+		  	</div>
+		  	<div v-else>
+		  		보관함
+		  	</div>
+  		</div>
+
+  		
+  	</div>
+  	
   </div>
+  
+  
 </template>
 
 <script>
+	import TabMenuComponent from './components/TabMenuComponent.vue'
+
 export default {
+	name :'app',
   data () {
-    return {}
+    return {
+    	tabsMenu : ['Favorite', 'Storage', 'Message'],
+    	selectedTabsMenu : '',
+    }
+  },
+  components :{
+  	'tabs-menu' : TabMenuComponent,
+  },
+  created(){
+  	this.selectedTabsMenu = this.tabsMenu[0]
+  },
+  methods:{
+  	onClickTabsMenu(tabsMenu){
+  		this.selectedTabsMenu = tabsMenu
+  	},
   }
+
 }
 </script>
-
-<style lang="scss" scoped>
-p {
-  font-size: 20px;
-}
-</style>
